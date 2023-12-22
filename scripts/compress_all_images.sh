3c18f40c81ef040fe3c1a0ff8f1837510ef977d6
Constant DESCHIETERE <constant@cn-ds.fr>
2023-12-22 17:51:50 +0100

    Updating images for better lighthouse reports

--- scripts/compress_all_images.sh
+++ scripts/compress_all_images.sh
@@ -2,5 +2,7 @@
 for f in ../assets/img/*.jpg;
 do 
     echo "Processing $f file..."; 
-    ./cwebp -q 50 $f -o $f.webp
+    ./cwebp -q 50 $f -o $f-lg.webp
+    ./cwebp -resize 480 0 -q 50 $f -o $f.webp
+    ./cwebp -resize 120 0 -q 50 $f -o $f-mini.webp
 done
